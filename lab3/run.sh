8906803a7fd5a2534d9d2f716839ba68ea435791
Paweł Mendroch <pawelmendroch7@gmail.com>
2020-10-25 11:11:19 +0100

    Finish lab3

--- lab3/run.sh
+++ lab3/run.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# for imageId in 06
-# do
-#     # echo "Running: img_00${imageId}_mask_Unet_toe.png"
-#     python lab3.py "img_00${imageId}_mask_Unet_toe.png"
-# done
-
 for imageId in 06 07 09 10 12 15 16 20 21 27
 do
-    # echo "Running: img_00${imageId}_mask_Unet_toe.png"
+    echo "Running: img_00${imageId}_mask_Unet_toe.png"
     python lab3.py "img_00${imageId}_mask_Unet_toe.png"
 done
